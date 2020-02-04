d4a6a714393a693fe96cd788af64e9834e8acf9d
sophie27 <sophie_fosson@yahoo.it>
2020-02-04 15:05:05 +0100

    Update and rename CSI_TV_v3.1_2020.cpp to CSI_1/CSI_v3.1_2020.cpp

--- CSI_1/CSI_v3.1_2020.cpp
+++ CSI_1/CSI_v3.1_2020.cpp
@@ -1,7 +1,7 @@
 /* 7 JAN 2020 */
 /** Code written by Sophie M. Fosson **/
-/** Compressed System Identification via online iterative soft thresholding, Douglas-Rachford splitting (ADMM), Distributed iterative soft thresholding **/
-// Time varying parameters evoving as STEPWISE CONSTANT functions
+/** Compressed system identification (CSI) via online iterative soft thresholding, Douglas-Rachford splitting (ADMM), Distributed iterative soft thresholding **/
+// Experiment CSI_1: Time varying parameters evoving as STEPWISE CONSTANT functions
 
 #include <stdio.h>
 #include <string.h>
@@ -19,7 +19,6 @@ using namespace std;
 using namespace Eigen;
 #define T 1000
 #define LAMBDA 1e-2
-//#define TAU 8e-3
 #define MU 1e-6
 #define pi 3.1415926535897932384626433832795028841971
 
@@ -27,7 +26,6 @@ double q, SNR, MAE_couple, MAE, TOT_MAE, thresh, thresh_dist,  np, admitted_run_
 double alpha0, alpha1, alpha, gap, gap2, fopt, fist, fadmm, fdista, dinreg_ist, dinreg_admm, dinreg_dista, dinreg_ist_theo, dinreg_admm_theo, dinreg_dista_theo;
 int N,M,K,numNodes,v,h,i,j,m,n, k,t, run, num_neighbors, measuresNode;
 
-// required functions
 double Gaussian_Noise(double std) {
     return std*(sqrt(-2*log( ((double) rand() / (RAND_MAX))  )))*cos(2*pi* ((double) rand() / (RAND_MAX))  );
 }
@@ -47,22 +45,22 @@ double soft_thresholding(double v,  double thresh) {
 int main (int argc, char *argv[]) {
 	if (argc < 5) {
 		cerr << "Usage: " << argv[0] << "  <N> <K> <M> <run> <noise power>" << endl;
-        cerr << "Ex: ./dynreg 20 2 15 1 0.01" << endl;
+        cerr << "Ex: ./dynreg 20 2 12 1 0.01" << endl;
 		return EXIT_FAILURE;
 	}
-    N = atoi (argv[1]);
+   	N = atoi (argv[1]);
 	K = atoi (argv[2]);
 	M = atoi (argv[3]);
-	run=atoi(argv[4]);
-	np=atof(argv[5]);
-	admitted_run_time=M/2; //6 ms
-	q=5e-1;	
+	run = atoi(argv[4]);
+	np = atof(argv[5]);
+	admitted_run_time = M/2; //6 ms
+	q = 5e-1;	
 	n = N/2;
 	numNodes = M/3; // number of nodes for DISTA
 	srand(10*run*M);
 	VectorXd y,  xtrue, xist, xadmm,  z, dual, noise, auxist, auxadmm, aux, u, xmdista[numNodes], xdista[numNodes], final_mean, zc, xadmmc, dualc, xprevious, auxs, previousglobal, TAUs;
 	MatrixXd A, A_T,B, As[numNodes], As_T[numNodes]; 
-    A.resize(M,N);
+    	A.resize(M,N);
 	A_T.resize(N,M);
 	B.resize(N,N);
  	xtrue.resize(N);
@@ -81,40 +79,36 @@ int main (int argc, char *argv[]) {
 	dualc.resize(N);
 	xprevious.resize(N);
  	TAUs.resize(numNodes);
-    measuresNode = M/numNodes;
+    	measuresNode = M/numNodes;
 
-    
-	
-	// initialize
+	/* Initialization */
 
-    // control input
+    	/* Input */
 	for (t = 0; t < M; t++)    {
 		u(t)=Gaussian_Noise(1);
         noise(t)= Gaussian_Noise(np);
 	}
 	for (t = M; t < T; t++)    {
-        	//u(t)=(2*(rand() % 2)-1); //a binary sequence
-        	//u(t)= (rand() % 2); //a binary sequence
 		u(t)=u(t % M);
-        noise(t)=Gaussian_Noise(np);
+        	noise(t)=Gaussian_Noise(np);
 	}
 
 	xtrue.setZero();
 	y.setZero();
 
-    xist.setZero();
-    xadmm.setZero();
-    z.setZero();
-    dual.setZero();
+    	xist.setZero();
+    	xadmm.setZero();
+    	z.setZero();
+    	dual.setZero();
 
-    for (j = 0; j < numNodes; j++) {
+    	for (j = 0; j < numNodes; j++) {
  		As[j].resize(measuresNode, N);
  		xdista[j].resize (N);
  		xmdista[j].resize (N);
  	}
     
     	
-    // parameters to identify
+    	/* Parameters to identify */
 	for (t = 1; t < T; t++)    {
 		// a
 		if (t < 0.3*T)
@@ -137,14 +131,13 @@ int main (int argc, char *argv[]) {
 		else
 			xtrue(n)= -0.7;
 
-        y(t) = xtrue(0)*y(t-1) + xtrue(n)*u(t-1)+ noise(t);
-    }
+        	y(t) = xtrue(0)*y(t-1) + xtrue(n)*u(t-1)+ noise(t);
+    	}
 
-    
-    auto t1 = Clock::now();
+    	auto t1 = Clock::now();
 	auto t2 = Clock::now();
 
-	// MAIN CYCLE
+	/* MAIN CYCLE */
 	for (t = 1; t < T-M; t += M)   {
 
 		// a
@@ -157,7 +150,6 @@ int main (int argc, char *argv[]) {
 		else
 			xtrue(0)= 0.9;
 
-		/*****/
 		// b
 		if (t < 0.2*T)
 			xtrue(n)=  0.7;
@@ -168,26 +160,25 @@ int main (int argc, char *argv[]) {
 		else
 			xtrue(n)= -0.7;
 
-
-        // fill A (A is Toeplitz)
+        	// fill A (A is Toeplitz)
 		for (i = 0; i < M; i++)  {
-		    for (j = 0; j < n; j++)  {
-                if (i+t-j-1 <0) {
-                    A(i,j)=0;
-                    A(i,j+n)=0;
-                }
-                else {
-                	A(i,j)=y(i+t-j-1);
-                	A(i,j+n)=u(i+t-j-1);
-            	}
+			for (j = 0; j < n; j++)  {
+                		if (i+t-j-1 <0) {
+                    			A(i,j)=0;
+                    			A(i,j+n)=0;
+                		}
+                		else {
+                			A(i,j)=y(i+t-j-1);
+                			A(i,j+n)=u(i+t-j-1);
+            			}
+        		}
         	}
-        }
-        A=1./sqrt(M)*A;
-        A_T=A.transpose();
+        	A=1./sqrt(M)*A;
+        	A_T=A.transpose();
 		TAU=2./(A.squaredNorm());
 		thresh =  LAMBDA*TAU;
-        thresh_dist =  LAMBDA*TAU/2;
-        for (j = 0; j < numNodes; j++) {
+        	thresh_dist =  LAMBDA*TAU/2;
+        	for (j = 0; j < numNodes; j++) {
  			for (i = 0; i < measuresNode; i++) {
 				for (h=0; h< N; h++) {
 					As[j](i,h)= sqrt(M)/sqrt(measuresNode)*A(j*measuresNode+i, h) ;
@@ -197,20 +188,19 @@ int main (int argc, char *argv[]) {
 			TAUs(j)=2./(As[j].squaredNorm());
  		}
 		        
-        /** Compute GLOBAL MINIMUM to compate**/
-        B=A_T*A+(1+MU)* MatrixXd::Identity(N, N);
+        	/** Compute GLOBAL MINIMUM (e.g., with ADMM) to compare**/
+        	B=A_T*A+(1+MU)* MatrixXd::Identity(N, N);
 		B=B.inverse();
 		for (k = 0; k < 1e6; k++)  {   
-        	xadmmc=B*(A_T*1./sqrt(M)*y.segment(t,M) + zc - dualc);			
+       		 	xadmmc=B*(A_T*1./sqrt(M)*y.segment(t,M) + zc - dualc);			
             
-            for (j = 0; j < N; j++) {
+           		for (j = 0; j < N; j++) {
 				zc(j)=soft_thresholding(xadmmc(j)+dualc(j), LAMBDA);
 			}
 			dualc=dualc+xadmmc-zc;
 			aux = xadmmc - xprevious;
 			xprevious = xadmmc;
 			if (aux.squaredNorm() < 1e-12) {
-				//t2 = Clock::now();
 				break;
 			}
 		}
@@ -223,7 +213,7 @@ int main (int argc, char *argv[]) {
 		}
 		previousglobal=xadmmc;
         
-        if ( t > 1 ) {
+        	if ( t > 1 ) {
  			auxs= y.segment(t,M) - sqrt(M)*A*xist;
 			fist = 0.5*auxs.squaredNorm()+ MU*0.5*xist.squaredNorm()+LAMBDA*xist.lpNorm<1>();
 			dinreg_ist = dinreg_ist + fist - fopt;
@@ -251,7 +241,7 @@ int main (int argc, char *argv[]) {
 			fdista = fdista + aux.squaredNorm()/3;
 			aux = xmdista[1]-xdista[0];
 			fdista = fdista + aux.squaredNorm()/3;
-			auxs= y.segment(t+0*measuresNode,measuresNode) -sqrt(measuresNode)*As[0]*xdista[0];
+			auxs = y.segment(t+0*measuresNode,measuresNode) -sqrt(measuresNode)*As[0]*xdista[0];
 			fdista =  fdista + 0.5*auxs.squaredNorm()+ (MU*0.5*xdista[0].squaredNorm()+LAMBDA*xdista[0].lpNorm<1>())/numNodes; 
 
 			aux = xmdista[numNodes-1]-xdista[numNodes-1];
@@ -260,38 +250,35 @@ int main (int argc, char *argv[]) {
 			fdista = fdista + aux.squaredNorm()/3;
 			aux = xmdista[0]-xdista[numNodes-1];
 			fdista = fdista + aux.squaredNorm()/3;
-			auxs= y.segment(t+(numNodes-1)*measuresNode,measuresNode) -sqrt(measuresNode)*As[numNodes-1]*xdista[numNodes-1];
+			auxs = y.segment(t+(numNodes-1)*measuresNode,measuresNode) -sqrt(measuresNode)*As[numNodes-1]*xdista[numNodes-1];
 			fdista =  fdista + 0.5*auxs.squaredNorm()+ (MU*0.5*xdista[numNodes-1].squaredNorm()+LAMBDA*xdista[numNodes-1].lpNorm<1>())/numNodes; 
 
 			dinreg_dista = dinreg_dista + fdista - fopt;
-
  		}
 
-         /** IST**/
-        t1 = Clock::now();
+        	/** Online IST **/
+      		t1 = Clock::now();
 		for (k = 0; k < 1e6; k++)  {
-        	xist=xist+TAU*A_T*(1./sqrt(M)*y.segment(t,M)-A*xist)-TAU*MU*xist;      
-            		
+        		xist=xist+TAU*A_T*(1./sqrt(M)*y.segment(t,M)-A*xist)-TAU*MU*xist;      	
 			for (j = 0; j < N; j++) {
 				xist(j)=soft_thresholding(xist(j), thresh);
 			}
-			
 			t2 = Clock::now();
 			if (std::chrono::duration_cast<std::chrono::nanoseconds>(t2 - t1).count() /1e6 > admitted_run_time) {
 				break;
 			}
-    	}
+    		}
     	 
- 		/** DOUGLAS-RACHFORD (equivalent to ADMM) **/
+ 		/** Online DOUGLAS-RACHFORD (DR; equivalent to ADMM) **/
  		// N.B. The notation "dual" and "admm" comes from previous ADMM-based version; however, this is DR (to be modified) 
  		t1 = Clock::now();
  		B=A_T*A+(1+MU)* MatrixXd::Identity(N, N);
 		B=B.inverse();
         
-        for (k = 0; k < 1e6; k++)  {   
-        	xadmm=B*(A_T*1./sqrt(M)*y.segment(t,M)+dual);			
+       	 	for (k = 0; k < 1e6; k++)  {   
+        		xadmm=B*(A_T*1./sqrt(M)*y.segment(t,M)+dual);			
             
-            for (j = 0; j < N; j++) {
+            		for (j = 0; j < N; j++) {
 				z(j)=soft_thresholding(2*xadmm(j)-dual(j), LAMBDA);
 			}
 			dual=dual+2*(-xadmm+z);
@@ -302,8 +289,8 @@ int main (int argc, char *argv[]) {
 			} 		
 		}
 		
-		/** DISTA**/
-        t1 = Clock::now();
+		/** Online DISTA **/
+        	t1 = Clock::now();
 		for (k = 0; k < 1e6; k++)  { 
 			// FIRST ROUND: COMPUTE THE LOCAL MEANS
 			for (v = 1; v < numNodes -1; v++) {
@@ -337,20 +324,14 @@ int main (int argc, char *argv[]) {
 				xdista[numNodes-1](j)=soft_thresholding(xdista[numNodes-1](j), LAMBDA*TAUs(numNodes-1)/(2));
 			}
 
-			
 			t2 = Clock::now();
 			if (std::chrono::duration_cast<std::chrono::nanoseconds>(t2 - t1).count() /(1e6*numNodes) > admitted_run_time) {
-				//cout << k <<" ehi dista"<< endl;
 				break;
 			}
-    	}
+    		}
 
-
-
-		/* output */
 		// At each time step t, I print the result
-		// I do not save with the script the variables over all time 
-
+	
 		auxist= xist-xtrue;
 		auxadmm= xadmm-xtrue;
 		auxdista=0;
@@ -365,15 +346,14 @@ int main (int argc, char *argv[]) {
 		
 		for (i = t; i < t+M; i++)    { 
 			if (i+M+admitted_run_time < 0.3*T)
-					xtrue(0)= -0.9;
+				xtrue(0)= -0.9;
 			else if (i+M+admitted_run_time < 0.5*T)
 				xtrue(0)= -0.9;
 			else if (i+M+admitted_run_time < 0.7*T)
 				xtrue(0)= 0.9;
 			else
 				xtrue(0)= 0.9;
-
-			// b
+			
 			if (i+M+admitted_run_time < 0.2*T)
 				xtrue(n)=  0.7;
 			else if (i+M+admitted_run_time < 0.4*T)
@@ -383,43 +363,42 @@ int main (int argc, char *argv[]) {
 			else
 				xtrue(n)= -0.7;
 	    		
-	    	cout << i+M+admitted_run_time << " "<<xtrue(0) << " " << xist(0) << " " << xtrue(n) << " " << xist(n) << "  ";
-	    	// I write the estimation of the null parameters
-        	for (j = 1; j < n; j++)    {    
-            	cout << xist(j) << "  ";
-        	}
-        	for (j = n+1; j < N; j++)    {    
-        		cout << xist(j) << "  ";
-        	}
-        	//... and to conclude the MSE
-        	cout << (auxist.squaredNorm()) / N <<  "     ";
+	    		cout << i+M+admitted_run_time << " "<<xtrue(0) << " " << xist(0) << " " << xtrue(n) << " " << xist(n) << "  ";
+	    		// I write the estimation of the null parameters
+        		for (j = 1; j < n; j++)    {    
+	            		cout << xist(j) << "  ";
+        		}
+        		for (j = n+1; j < N; j++)    {    
+        			cout << xist(j) << "  ";
+        		}
+        		//... and to conclude the MSE
+ 		       	cout << (auxist.squaredNorm()) / N <<  "     ";
 
                
-	    	cout << i +M+admitted_run_time << " "<<xtrue(0) << " " << xadmm(0) << " " << xtrue(n) << " " << xadmm(n) << "  ";
-	    	// I write the estimation of the null parameters
-        	for (j = 1; j < n; j++)    {    
-        		cout << xadmm(j) << "  ";
-        	}
-        	for (j = n+1; j < N; j++)    {    
-        		cout << xadmm(j) << "  ";
-        	}
-        	//... and to conclude the MSE
-        	cout << (auxadmm.squaredNorm()) / N <<  "     ";
+		    	cout << i +M+admitted_run_time << " "<<xtrue(0) << " " << xadmm(0) << " " << xtrue(n) << " " << xadmm(n) << "  ";
+		    	// I write the estimation of the null parameters
+       		 	for (j = 1; j < n; j++)    {    
+     	   			cout << xadmm(j) << "  ";
+        		}
+        		for (j = n+1; j < N; j++)    {    
+        			cout << xadmm(j) << "  ";
+        		}
+        		//... and to conclude the MSE
+        		cout << (auxadmm.squaredNorm()) / N <<  "     ";
 
 
 			cout << i +M+admitted_run_time << " "<<xtrue(0) << " " << final_mean(0) << " " << xtrue(n) << " " << final_mean(n) << "  ";
-	    	// I write the estimation of the null parameters
-        	for (j = 1; j < n; j++)    {    
-            	cout << final_mean(j) << "  ";
-        	}
-        	for (j = n+1; j < N; j++)    {    
-            	cout << final_mean(j) << "  ";
-        	}
-        	//... and to conclude the MSE
-         	cout << auxdista / N <<  "     ";
-         	cout << dinreg_ist_theo << "  " << dinreg_ist << "  " << dinreg_admm << "  " << dinreg_dista << "  "<<endl;
-    	} // end i
-	} //end time t
-		
+	    		// I write the estimation of the null parameters
+        		for (j = 1; j < n; j++)    {    
+        	 	   	cout << final_mean(j) << "  ";
+        		}
+        		for (j = n+1; j < N; j++)    {    
+            			cout << final_mean(j) << "  ";
+        		}
+        		//... and to conclude the MSE
+         		cout << auxdista / N <<  "     ";
+         		cout << dinreg_ist_theo << "  " << dinreg_ist << "  " << dinreg_admm << "  " << dinreg_dista << "  "<<endl;
+    		} // end i
+	} //end time t		
 	return EXIT_SUCCESS;
 }
